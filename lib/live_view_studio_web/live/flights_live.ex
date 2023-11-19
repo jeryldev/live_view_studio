ada8535ab7863b8967bbbe0273bd881a5077dfed
jeryldev <jeryldev@gmail.com>
2023-11-19 22:50:32 +0800

    add handle_event to search the airport and to update the flights

--- lib/live_view_studio_web/live/flights_live.ex
+++ lib/live_view_studio_web/live/flights_live.ex
@@ -1,6 +1,8 @@
 defmodule LiveViewStudioWeb.FlightsLive do
   use LiveViewStudioWeb, :live_view
 
+  alias LiveViewStudio.Flights
+
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
@@ -15,7 +17,7 @@ defmodule LiveViewStudioWeb.FlightsLive do
     ~H"""
     <h1>Find a Flight</h1>
     <div id="flights">
-      <form>
+      <form phx-submit="search">
         <input
           type="text"
           name="airport"
@@ -32,29 +34,39 @@ defmodule LiveViewStudioWeb.FlightsLive do
 
       <div class="flights">
         <ul>
-          <%= for flight <- @flights do %>
-            <li>
-              <div class="first-line">
-                <div class="number">
-                  Flight #<%= flight.number %>
-                </div>
-                <div class="origin-destination">
-                  <%= flight.origin %> to <%= flight.destination %>
-                </div>
+          <%!-- <%= for flight <- @flights do %> --%>
+          <li :for={flight <- @flights}>
+            <div class="first-line">
+              <div class="number">
+                Flight #<%= flight.number %>
               </div>
-              <div class="second-line">
-                <div class="departs">
-                  Departs: <%= flight.departure_time %>
-                </div>
-                <div class="arrives">
-                  Arrives: <%= flight.arrival_time %>
-                </div>
+              <div class="origin-destination">
+                <%= flight.origin %> to <%= flight.destination %>
               </div>
-            </li>
-          <% end %>
+            </div>
+            <div class="second-line">
+              <div class="departs">
+                Departs: <%= flight.departure_time %>
+              </div>
+              <div class="arrives">
+                Arrives: <%= flight.arrival_time %>
+              </div>
+            </div>
+          </li>
+          <%!-- <% end %> --%>
         </ul>
       </div>
     </div>
     """
+  end
+
+  def handle_event("search", %{"airport" => airport}, socket) do
+    socket =
+      assign(socket,
+        airport: airport,
+        flights: Flights.search_by_airport(airport)
+      )
+
+    {:noreply, socket}
   end
 end
