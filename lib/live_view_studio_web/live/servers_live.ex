8c2b7b4e4c97e2f61d90a3c8288b40ba3c06ec76
jeryldev <jeryldev@gmail.com>
2024-11-08 10:45:05 +0800

    Exercise: Live Validate the Server Form

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -68,18 +68,27 @@ defmodule LiveViewStudioWeb.ServersLive do
       <div class="main">
         <div class="wrapper">
           <%= if @live_action == :new do %>
-            <.form for={@form} phx-submit="save">
+            <.form for={@form} phx-submit="save" phx-change="validate">
               <div class="field">
-                <.input field={@form[:name]} placeholder="Name" />
+                <.input
+                  field={@form[:name]}
+                  placeholder="Name"
+                  phx-debounce="2000"
+                />
               </div>
               <div class="field">
-                <.input field={@form[:framework]} placeholder="Framework" />
+                <.input
+                  field={@form[:framework]}
+                  placeholder="Framework"
+                  phx-debounce="blur"
+                />
               </div>
               <div class="field">
                 <.input
                   field={@form[:size]}
                   placeholder="Size (MB)"
                   type="number"
+                  phx-debounce="blur"
                 />
               </div>
               <.button phx-disable-with="Saving...">
@@ -162,5 +171,14 @@ defmodule LiveViewStudioWeb.ServersLive do
       {:error, changeset} ->
         {:noreply, assign(socket, :form, to_form(changeset))}
     end
+  end
+
+  def handle_event("validate", %{"server" => server_params}, socket) do
+    changeset =
+      %Server{}
+      |> Servers.change_server(server_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :form, to_form(changeset))}
   end
 end
