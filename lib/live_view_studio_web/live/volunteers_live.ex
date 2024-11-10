474564bb99d28d8d7a5eab394aedf6a1903dcf72
jeryldev <jeryldev@gmail.com>
2024-11-10 17:14:33 +0800

    Move the form component and its events, to its own live_component module. Send a message to the volunteers liveview to update the stream of volunteers.

--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -2,26 +2,19 @@ defmodule LiveViewStudioWeb.VolunteersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Volunteers
-  alias LiveViewStudio.Volunteers.Volunteer
 
   def mount(_params, _session, socket) do
-    volunteers = Volunteers.list_volunteers()
-
-    changeset = Volunteers.change_volunteer(%Volunteer{})
-
-    socket =
-      socket
-      |> stream(:volunteers, volunteers)
-      |> assign(:form, to_form(changeset))
-
-    {:ok, socket}
+    {:ok, stream(socket, :volunteers, Volunteers.list_volunteers())}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
-      <.volunteer_form form={@form} />
+      <.live_component
+        module={LiveViewStudioWeb.VolunteerFormComponent}
+        id={:new}
+      />
       <pre>
         <%#= inspect(@form, pretty: true) %>
       </pre>
@@ -33,27 +26,6 @@ defmodule LiveViewStudioWeb.VolunteersLive do
         />
       </div>
     </div>
-    """
-  end
-
-  def volunteer_form(assigns) do
-    ~H"""
-    <.form for={@form} phx-submit="save" phx-change="validate">
-      <.input
-        field={@form[:name]}
-        placeholder="Name"
-        autocomplete="off"
-        phx-debounce="2000"
-      />
-      <.input
-        field={@form[:phone]}
-        type="tel"
-        placeholder="Phone"
-        autocomplete="off"
-        phx-debounce="blur"
-      />
-      <.button phx-disable-with="Saving...">Check In</.button>
-    </.form>
     """
   end
 
@@ -88,6 +60,10 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     """
   end
 
+  def handle_info({:volunteer_created, volunteer}, socket) do
+    {:noreply, stream_insert(socket, :volunteers, volunteer, at: 0)}
+  end
+
   def handle_event("delete", %{"id" => id}, socket) do
     volunteer = Volunteers.get_volunteer!(id)
 
@@ -102,31 +78,5 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     {:ok, volunteer} = Volunteers.toggle_status_volunteer(volunteer)
 
     {:noreply, stream_insert(socket, :volunteers, volunteer)}
-  end
-
-  def handle_event("validate", %{"volunteer" => volunteer_params}, socket) do
-    changeset =
-      %Volunteer{}
-      |> Volunteers.change_volunteer(volunteer_params)
-      |> Map.put(:action, :validate)
-
-    {:noreply, assign(socket, :form, to_form(changeset))}
-  end
-
-  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
-    case Volunteers.create_volunteer(volunteer_params) do
-      {:ok, volunteer} ->
-        socket = stream_insert(socket, :volunteers, volunteer, at: 0)
-
-        changeset = Volunteers.change_volunteer(%Volunteer{})
-
-        {:noreply,
-         socket
-         |> assign(:form, to_form(changeset))
-         |> put_flash(:info, "Volunteer successfully checked in!")}
-
-      {:error, changeset} ->
-        {:noreply, assign(socket, :form, to_form(changeset))}
-    end
   end
 end
