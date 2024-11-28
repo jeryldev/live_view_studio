a9f3536970ea2ee06c33c7d94d0efe4bf8f30dc2
jeryldev <jeryldev@gmail.com>
2024-11-28 12:08:47 +0800

    Exercise: Copy to Clipboard

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -1,9 +1,11 @@
 import Calendar from "./calendar"
+import CustomClipboard from "./custom_clipboard"
 import PhoneNumber from "./phone_number"
 
 let Hooks = {
   Calendar: Calendar,
-  PhoneNumber: PhoneNumber
+  PhoneNumber: PhoneNumber,
+  Clipboard: CustomClipboard
 }
 
 export default Hooks
